c14aa098ba753c042870d805fd3c22871fca5258
agui <55447192+GuiWonder@users.noreply.github.com>
2022-08-01 04:00:53 -0500

    Update and rename test.bat to BuildAll.bat

--- main/test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python sourcehantocl.py SourceHanSerif-Bold.otf SourceHanSerif-cl-Bold.otf y 3 2 y 1
-python sourcehantocl.py SourceHanSerif-ExtraLight.otf SourceHanSerif-cl-ExtraLight.otf y 3 2 y 1
-python sourcehantocl.py SourceHanSerif-Heavy.otf SourceHanSerif-cl-Heavy.otf y 3 2 y 1
-python sourcehantocl.py SourceHanSerif-Light.otf SourceHanSerif-cl-Light.otf y 3 2 y 1
-python sourcehantocl.py SourceHanSerif-Medium.otf SourceHanSerif-cl-Medium.otf y 3 2 y 1
-python sourcehantocl.py SourceHanSerif-Regular.otf SourceHanSerif-cl-Regular.otf y 3 2 y 1
-python sourcehantocl.py SourceHanSerif-SemiBold.otf SourceHanSerif-cl-SemiBold.otf y 3 2 y 1
-pause
